9847e24939eb6afe4a98a200eb5be16ab169f833
nitika <dushnitisi9761@gmail.com>
2026-04-16 22:28:14 +0530

    refactor server.js to use single quotes and change PORT declaration to let

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
 
 const server = express();
-const PORT = 3000;
+let PORT = 3000;
+
+mongoose.connect()
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
