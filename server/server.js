8510f5a51531dfae35f130f8ea5113983244ac54
nitika <dushnitisi9761@gmail.com>
2026-04-17 22:58:24 +0530

    add .env file for database connection and update mongoose connection in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const server = express();
 let PORT = 3000;
 
-mongoose.connect()
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true
+})
+
+server.post('/signup', (req, res) => {
+  res.json(req.body)
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
